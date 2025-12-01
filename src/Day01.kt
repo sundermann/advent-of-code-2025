d6ad1516503bd6709081801d2a96421f888fb42e
Stephan Sundermann <stephansundermann@gmail.com>
2025-12-01 19:21:34 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,33 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.runningFold(50) { acc, s ->
+            val op = s.first()
+            val amount = s.drop(1).toInt()
+            val next = if (op == 'L') acc - amount else acc + amount
+            next.mod(100)
+        }.count { it == 0 }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var result = 0
+        input.runningFold(50) { acc, s ->
+            val op = s.first()
+            val amount = s.drop(1).toInt()
+            val shift = amount % 100
+            val next = if (op == 'L') acc - shift else acc + shift
+            if (acc != 0 && next !in (1..99))
+                result++
+            result += amount / 100
+            next.mod(100)
+        }.count { it == 0 }
+        return result
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
+    println(part2(testInput))
+    check(part2(testInput) == 6)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
